dbb34287cc926e2ba029e7269736bc2db9637f86
Ken Lu <ken@seeice.com>
2015-06-07 22:12:26 -0400

    [refctor] delegate and acts as statemachine

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
+  include AASM
 
   CLEARANCE_PRICE_PERCENTAGE  = BigDecimal.new("0.75")
 
@@ -6,19 +7,38 @@ class Item < ActiveRecord::Base
   belongs_to :clearance_batch
   validates :size, presence: true
 
+  delegate :type, :name, :min_sale_price, :wholesale_price, :retail_price, to: :style, prefix: true
+
   scope :sellable, -> { where(status: 'sellable') }
   scope :sort_by, -> (term) {order(term.to_sym) if term.present? }
 
-  def clearance!
+  def update_sold_price
     price_sold = style.wholesale_price * CLEARANCE_PRICE_PERCENTAGE
     if ["Pants", "Dress"].include?(style.type) and price_sold < 5
       price_sold = 5
     elsif price_sold < 2
       price_sold = 2
     end
-
-    update_attributes!(status: 'clearanced', 
-                       price_sold: price_sold)
+    self.price_sold = price_sold
+    self.sold_at = Time.now
   end
 
+  aasm :column => :status do
+    state :sellable, :initial => true
+    state :not_sellable
+    state :sold
+    state :clearanced
+
+    event :clearance, after: :update_sold_price do
+      transitions :from => :sellable, :to => :clearanced
+    end
+
+    event :restock do
+      transitions :from => [:clearanced, :sold], :to => :sellable
+    end
+
+    event :sell do
+      transitions :from => :sellable, :to => :sold
+    end
+  end
 end
